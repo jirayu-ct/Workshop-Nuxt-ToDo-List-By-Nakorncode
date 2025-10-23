55dbeada158d089bc89036f6e5afddf44fb86b1c
jirayu <125994181+jirayu-ct@users.noreply.github.com>
2025-10-23 15:17:27 +0700

    Add login page and update navigation links
    Introduces a new login page with email and password authentication, including toast notifications for success and error states. Updates the default layout to add a 'Login' link to the navigation bar.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,7 +4,8 @@
             <div class="max-w-md mx-auto bg-gray-100 p-2 rounded-lg mb-4 showdow-lg">
                 <ul class="flex items-center gap-2">
                     <li><nuxt-link to="/" class="font-bold text-lg text-blue-600 underline">Todo List</nuxt-link></li>
-                    <li  class="ml-auto"><nuxt-link to="/sign-up" class="text-white bg-blue-600 px-2 py-1 rounded-md hover:bg-blue-800">Sign Up</nuxt-link></li>
+                    <li class="ml-auto"><nuxt-link to="/login" class="text-blue underline">Login</nuxt-link></li>
+                    <li><nuxt-link to="/sign-up" class="text-white bg-blue-600 px-2 py-1 rounded-md hover:bg-blue-800">Sign Up</nuxt-link></li>
                 </ul>
             </div>
         </nav>
